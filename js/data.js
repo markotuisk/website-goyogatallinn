61b31e411542a635078a20c9b0a452243251b8dc
marko tuisk <markotuisk@markos-MacBook-Pro.local>
2026-03-05 03:44:28 +0000

    Update Ashtanga subpage header image to the new uploaded 2- image

--- js/data.js
+++ js/data.js
@@ -274,7 +274,7 @@ window.teachersData = teachersData;
 const classesData = {
     "ashtanga-yoga": {
         icon: "activity", // lucid icon name
-        image: "https://images.unsplash.com/photo-1599447421416-3414500d18a5",
+        image: "assets/classes/2-ashtanga-goyoga-tallinn-estonia-jooga-stuudio-narvamnt7d-raili-maripuu-keiu-grossberg-victooria-pajusaar-.webp.webp",
         teacherIds: ["raili-maripuu", "victoria-palusaar", "keiu-grossberg"],
         translations: {
             en: {
